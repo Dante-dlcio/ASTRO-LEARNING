f8cced3183b060983f82d4b658dc19928b6d66c3
dante-dlcio <erio-donalicio@hotmail.com>
2024-04-12 11:25:38 +0200

    landing page intro on the works

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,9 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      backgroundImage:{
+        'spaceXbg':"url('./src/utils/spacex-background.jpg')"
+      },
       fontFamily: {
         spacex: ['SpaceX', 'sans-serif'],
       },
